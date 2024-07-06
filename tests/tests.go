bb1060ca0c8a4779f04e9f8d7439d176ec2bd35c
Jozef Reisinger <jozef.reisinger@gmail.com>
2024-07-06 19:06:02 +0200

    run tests on a host in parallel

--- tests/tests.go
+++ tests/tests.go
@@ -15,6 +15,13 @@ var Tests = map[string]func(host string, config []byte) (string, error){
 	"PodsNotRunning": PodsNotRunning,
 }
 
+type test struct {
+	host         string
+	name         string
+	failedReason string
+	err          error
+}
+
 func Run(configFile string, verbose bool) (failed int, err error) {
 	b, err := os.ReadFile(configFile)
 	if err != nil {
@@ -29,30 +36,49 @@ func Run(configFile string, verbose bool) (failed int, err error) {
 	}
 
 	for host, testsConfig := range config {
+		ch := make(chan test, len(testsConfig))
+
 		for testName, testConfig := range testsConfig {
 			testFunc, ok := Tests[testName]
 			if !ok {
 				return 0, fmt.Errorf("no such test: %s", testName)
 			}
 
-			failedReason, err := testFunc(host, testConfig)
-			if err != nil {
-				return 0, fmt.Errorf("run test %s against %s: %v", testName, host, err)
-			}
+			go func(host, testName string, testConfig []byte) {
+				var t test
+				t.host = host
+				t.name = testName
+				t.failedReason, t.err = testFunc(host, testConfig)
+				ch <- t
+			}(host, testName, testConfig)
+		}
 
-			if failedReason != "" {
+		for range testsConfig {
+			t := <-ch
+			if t.err != nil {
+				return 0, fmt.Errorf("run test %s against %s: %v", t.name, t.host, t.err)
+			}
+			if t.failedReason != "" {
 				failed++
-				msg := fmt.Sprintf("fail %s on %s", testName, host)
-				if verbose {
-					msg += fmt.Sprintf(": %s", failedReason)
-				}
-				fmt.Println(msg)
-			} else if verbose {
-				msg := fmt.Sprintf("ok   %s on %s", testName, host)
-				fmt.Println(msg)
+				printFail(t, verbose)
+			} else {
+				printOk(t, verbose)
 			}
 		}
 	}
 
 	return failed, nil
+}
+
+func printFail(t test, verbose bool) {
+	msg := fmt.Sprintf("fail %s on %s", t.name, t.host)
+	if verbose {
+		msg += fmt.Sprintf(": %s", t.failedReason)
+	}
+	fmt.Println(msg)
+}
+
+func printOk(t test, verbose bool) {
+	msg := fmt.Sprintf("ok   %s on %s", t.name, t.host)
+	fmt.Println(msg)
 }
